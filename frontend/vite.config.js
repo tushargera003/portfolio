351d9da562429d47238c14979de068cff2d6eeff
Tushar Gera <tushargera003@gmail.com>
2025-03-25 01:33:48 +0530

    updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: "",  
+  plugins: [react(), tailwindcss()],
+  base: "/",
   build: {
     outDir: "dist",
   },
-})
+  server: {
+    historyApiFallback: true, // Ensures that dynamic routes work
+  },
+  preview: {
+    historyApiFallback: true, // Required for Vercel preview
+  },
+});
